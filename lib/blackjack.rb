2e25f7de02fec0fedee386e87ef6bcddc6018704
Trent Moss <trentmoss05@gmail.com>
2019-01-02 03:19:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,12 +35,23 @@ def initial_round
   return first_round
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if input = 'h'
+    card_total += deal_card
+  elsif input = 's'
+    card_total
+  else
+    invalid_command
+    prompt_user
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
