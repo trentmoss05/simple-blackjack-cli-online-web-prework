581900db035f9a44d1ddb48f63554f4ac38f0815
Trent Moss <trentmoss05@gmail.com>
2019-01-02 03:22:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,4 +61,10 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
 end
